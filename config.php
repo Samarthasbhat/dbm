92ffb9570202790f69887df492e6acebfef731d6
manoj <manojgaonkar7648@gmail.com>
2022-02-10 17:42:12 +0530

    Completing contact.php

--- config.php
+++ config.php
@@ -1,20 +1,8 @@
 <?php
-//$servername = "localhost";
-//$database = "telecom";
-//$username = "root";
-//$password = "";
+$servername = "localhost";
+$user = "root";
+$database = "telecom";
+$password = "";
 
-// Create connection
-
-//$conn = mysqli_connect($servername, $username, $password, $database);
-
-// Check connection
-
-//if ($conn->connect_error) {
-//die("Connection failed: " . $conn->connect_error);
-//}
-
-echo “Connected successfully”;
-
-//mysqli_close($conn);
+$conn = new mysqli($servername, $user, $password, $database);
 ?>
--- contact.php
+++ contact.php
@@ -1,6 +1,5 @@
 <?php
 // Create connection
-
 $conn = new mysqli("localhost", "root", "", "telecom");
 
 // Check connection
@@ -8,25 +7,28 @@ $conn = new mysqli("localhost", "root", "", "telecom");
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
-else{
-  echo "Sup bitches";
-}
-echo "<p>Entered data is : " . "</p>";
-echo "First Name is : " . $_POST["first_name"] . "</p>";
-echo "Last Name is  : " . $_POST["last_name"] . "</p>";
-echo "Email is  : " . $_POST["email"] . "</p>";
-echo "Phone number is  : " . $_POST["phone"] . "</p>";
-echo "Comment : " . $_POST["comments"] . "</p>";
+
 $f_name = $_POST["first_name"];
 $l_name = $_POST["last_name"];
 $email = $_POST["email"];
 $phone = $_POST["phone"];
 $comment = $_POST["comments"];
-//$sql = "INSERT INTO `contact` (`first_name`, `last_name`, `email`, `phone_num`, `comment`) VALUES (\'$f_name\', \'$l_name\', \'$email\', \'$phone\', \'$comment\');";
-//echo "<p>" . $sql . "</p>";
-$sql = "INSERT INTO `contact` (`first_name`, `last_name`, `email`, `phone_num`, `comment`) VALUES ('shyamsundar', 'iyanger', 'slkfjsl@shanda.com', 'sdflksjdjflsd');";
+
+echo "<pre><p>First Name : " . $f_name . "</p></pre>";
+echo "<pre><p>Last Name  : " . $l_name . "</p></pre>";
+echo "<pre><h6>Email      : " . $email . "</h6></pre>";
+echo "<pre><p>Phone no   : " . $phone . "</p></pre>";
+echo "<pre><p>Comment    : " . $comment . "</p></pre>";
+$sql = "INSERT INTO `contact` (`first_name`, `last_name`, `email`, `phone_num`, `comment`) VALUES ('$f_name', '$l_name', '$email', '$phone','$comment');";
 if ($conn->query($sql) == TRUE) {
-  echo "New record created successfully";
+  print<<<EOL
+  <style>
+  #contactform{
+    display : none;
+  }
+  </style>
+  <div> <h3>Thank you, We'll reach you as soon as we can.</h3></div>
+  EOL;
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
