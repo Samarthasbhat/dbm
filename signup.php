771c5bf23a50e2f5965e6b17718d5c267be1cd92
Nachiketh Shetty <shettynachiketh2002@gmail.com>
2022-02-11 10:54:51 +0530

    okay

--- signup.php
+++ signup.php
@@ -4,23 +4,18 @@ $con=new mysqli("localhost","root","","telecom");
 if($con->connect_error){
  	die("connectionfaild");
 } 
-echo <<<EOL
-<style>
-#sign_up{
-	display : none;
-}
-</style>
-EOL
+
+
 $name =$_POST["name"];
 $email=$_POST["email"];
-$date=date("Y-M-D");
+$date=date("Y-m-d");
 $mphone=$_POST["mobile"];
 $phone=$_POST["home_phone"];
 $address=$_POST["address"];
 $password=$_POST["password"];
 $con_password=$_POST["con_password"];
 
-$sql = "INSERT INTO Customer(identity_card_no,name,email,registration_date,mobile_ph,home_ph,address,password) values ('5000','$name','$email','$date','$mphone','$phone','$address','$password')";
+$sql = "INSERT INTO Customer(identity_card_no,name,email,registration_date,mobile_ph,home_ph,address,password) values ('1','$name','$email','$date','$mphone','$phone','$address','$password')";
 
 echo "<p>Firstname= " .$name . "</p>";
 echo "<p>email= " .$email . "</p>";
