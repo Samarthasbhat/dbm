4983dd926cd6beeb89095d254f519628ddf6efe1
manoj <manojgaonkar7648@gmail.com>
2022-02-11 16:52:44 +0530

    Login working now

--- signup.php
+++ signup.php
@@ -8,30 +8,49 @@ if($con->connect_error){
 
 $name =$_POST["name"];
 $email=$_POST["email"];
-$date=$_POST["date"];
+// $date=$_POST["date"];
+$date=date("Y-m-d");
+
 $mphone=$_POST["mobile"];
 $phone=$_POST["home_phone"];
 $address=$_POST["address"];
 $password=$_POST["password"];
 $con_password=$_POST["con_password"];
 
-$sql = "INSERT INTO Customer(identity_card_no,name,email,registration_date,mobile_ph,home_ph,address,password) values ('1','$name','$email','$date','$mphone','$phone','$address','$password')";
+$sql = "INSERT INTO Customer(identity_card_no,name,email,registration_date,mobile_ph,home_ph,address,password) values (identity_card_no,'$name','$email','$date','$mphone','$phone','$address','$password')";
 
-echo "<p>Firstname= " .$name . "</p>";
-echo "<p>email= " .$email . "</p>";
-echo "<p>phone= " .$phone . "</p>";
-echo "<p>password =" .$password . "</p>";
-echo "<p>con_pass=" .$con_password . "</p>";
-echo "<p>phone= " .$mphone . "</p>";
-echo "<p>address= " .$address . "</p>";
-echo "<p>date= " .$date . "</p>";
+// echo "<p>Firstname= " .$name . "</p>";
+// echo "<p>email= " .$email . "</p>";
+// echo "<p>phone= " .$phone . "</p>";
+// echo "<p>password =" .$password . "</p>";
+// echo "<p>con_pass=" .$con_password . "</p>";
+// echo "<p>phone= " .$mphone . "</p>";
+// echo "<p>address= " .$address . "</p>";
+// echo "<p>date= " .$date . "</p>";
 
 if($con->query($sql) == TRUE){
-	echo "sup bitches";
+	echo <<<EOL
+	<style>
+	#sign_up{
+		display : none;
+	}
+	</style>
+	EOL;
+	echo "<center><h3 class = 'signup-done'>Thank you for signing up.</h3></center>";
 }
 else{
 	echo "needs work";
 }
 
+// mysql_query($sql) or die($sql . mysql_error());
+
+
 $con->close();
 ?>
+
+<style>
+	.signup-done{
+		color: #454545;
+
+	}
+</style>
